e26f3349a58034fa16bc53247858de8290d3795d
Oleksandr Zagovorychev <zagovorichev@gmail.com>
2022-05-09 14:28:37 +0200

    - added delete button to the filter blockquote map - fixed dashboard view for large screen

--- src/app/components/search/components/searcher/filters/searcher.filters.component.ts
+++ src/app/components/search/components/searcher/filters/searcher.filters.component.ts
@@ -20,6 +20,15 @@ import {Searcher} from '../../form/models/searcher';
 import {LoggerComponent} from '../../../../core/logger/LoggerComponent';
 import {GlobalState} from '../../../../../global.state';
 import {TranslateService} from '@ngx-translate/core';
+import {Doctor} from '../../../../doctors';
+import {Assistant} from '../../../../assistant';
+import {City} from '../../../../city';
+import {Patient} from '../../../../patient/patient';
+import {AccidentStatus} from '../../../../accident/components/status';
+import {AccidentType} from '../../../../accident/components/type/type';
+import {Survey} from '../../../../survey';
+import {Service} from '../../../../service';
+import {Diagnostic} from '../../../../diagnostic/diagnostic';
 
 @Component({
   selector: 'nga-searcher-filters',
@@ -37,5 +46,58 @@ export class SearcherFiltersComponent extends LoadingComponent {
     protected translateService: TranslateService,
   ) {
     super();
+  }
+
+  deleteCaseableType(type: string) {
+    this.searcher.filters.caseableTypes = this.searcher.filters.caseableTypes.filter((ct) => ct !== type);
+  }
+
+  deleteDoctor(id: number) {
+    this.searcher.filters.doctors = this.searcher.filters.doctors.filter((doc: Doctor) => doc.id !== id);
+  }
+
+  deleteAssistant(id: number) {
+    this.searcher.filters.assistants = this.searcher.filters.assistants
+      .filter((assistant: Assistant) => assistant.id !== id);
+  }
+
+  deleteCity(id: number) {
+    this.searcher.filters.cities = this.searcher.filters.cities
+      .filter((city: City) => city.id !== id);
+  }
+
+  deletePatient(id: number) {
+    this.searcher.filters.patients = this.searcher.filters.patients
+      .filter((patient: Patient) => patient.id !== id);
+  }
+
+  deleteHandlingTimeRange(handlingTimeRange: string) {
+    this.searcher.filters.handlingTimeRanges = this.searcher.filters.handlingTimeRanges
+      .filter((handlingRange: string) => handlingRange !== handlingTimeRange);
+  }
+
+  deleteAccidentStatus(id: number) {
+    this.searcher.filters.accidentStatuses = this.searcher.filters.accidentStatuses
+      .filter((accidentStatus: AccidentStatus) => accidentStatus.id !== id);
+  }
+
+  deleteAccidentType(id: number) {
+    this.searcher.filters.accidentTypes = this.searcher.filters.accidentTypes
+      .filter((accidentType: AccidentType) => accidentType.id !== id);
+  }
+
+  deleteDoctorService(id: number) {
+    this.searcher.filters.doctorServices = this.searcher.filters.doctorServices
+      .filter((service: Service) => service.id !== id);
+  }
+
+  deleteDoctorSurvey(id: number) {
+    this.searcher.filters.doctorSurveys = this.searcher.filters.doctorSurveys
+      .filter((survey: Survey) => survey.id !== id);
+  }
+
+  deleteDoctorDiagnostic(id: number) {
+    this.searcher.filters.doctorDiagnostics = this.searcher.filters.doctorDiagnostics
+      .filter((diagnostic: Diagnostic) => diagnostic.id !== id);
   }
 }
